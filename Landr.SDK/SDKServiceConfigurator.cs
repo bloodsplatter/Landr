4f0dedb0314772c922a899616032ac459115ab6e
Jonas Van der Aa <ketsukonn@gmail.com>
2018-12-11 21:05:30 +0100

    Autofac pt. 1
    An attempt at adding autofac to the SDK and integrating it with the web app

---
+++ Landr.SDK/SDKServiceConfigurator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Landr.SDK
+{
+    public sealed class SDKServiceConfigurator : IDisposable
+    {
+        public ContainerBuilder ContainerBuilder { get; }
+
+        public SDKServiceConfigurator(IServiceCollection services)
+        {
+            ContainerBuilder = new ContainerBuilder();
+            ContainerBuilder.Populate(services);
+        }
+
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
