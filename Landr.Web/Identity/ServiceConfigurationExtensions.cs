17c7bbaeabf16cdafeafe078fbf7e4f1608a1cc9
Jonas Van der Aa <ketsukonn@gmail.com>
2019-01-05 08:18:43 +0100

    added Identity to Landr.Web

--- Landr.Web/Identity/ServiceConfigurationExtensions.cs
+++ Landr.Web/Identity/ServiceConfigurationExtensions.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Landr.Web.Identity
@@ -10,6 +12,7 @@ namespace Landr.Web.Identity
     public static class ServiceConfigurationExtensions
     {
         private const string AllowedUsernameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        private const string UsersDbFileName = "users.db";
 
         public static void ConfigureIdentity(this IServiceCollection services)
         {
@@ -39,6 +42,17 @@ namespace Landr.Web.Identity
                 options.AccessDeniedPath = "/User/AccessDenied";
                 options.SlidingExpiration = true;
             });
+
+            //change this method (and the packages) to change the Identity store (i.e. to SQL Server)
+            DbContextOptionsBuilder CreateIdentityDbOptions(DbContextOptionsBuilder optionsBuilder)
+            {
+                return optionsBuilder.UseSqlite($"Data Source={UsersDbFileName}", options => options.MigrationsAssembly("Landr.Web"));
+            }
+
+            services.AddDbContext<IdentityDbContext>(options => CreateIdentityDbOptions(options));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
         }
     }
 }
