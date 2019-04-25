6aaf4cf11e23a6453123b2b5e5211aed78a6b85e
Jonas Van der Aa <ketsukonn@gmail.com>
2019-04-25 22:33:06 +0200

    Cleaned up using

--- Landr.Web/Controllers/AccountController.cs
+++ Landr.Web/Controllers/AccountController.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Build.Tasks;
 using Microsoft.Extensions.Localization;
 
 namespace Landr.Web.Controllers
