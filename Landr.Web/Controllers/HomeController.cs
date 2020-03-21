87ceb86886f4792399191b9467432875881fcb8a
Jonas Van der Aa <ketsukonn@gmail.com>
2020-03-21 19:01:27 +0100

    Web: pass apps to view

--- Landr.Web/Controllers/HomeController.cs
+++ Landr.Web/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Landr.SDK;
 
 namespace Landr.Web.Controllers
 {
     public class HomeController : Controller
     {
+        #region Fields
+        private readonly IAppProvider appProvider;
+        #endregion
+
+        #region Constructor
+        public HomeController(IAppProvider appProvider)
+        {
+            this.appProvider = appProvider;
+        }
+        #endregion
+
+        #region Actions
         public IActionResult Index()
         {
-            return View();
+            return View(appProvider.GetApps().ToArray());
         }
+
+        #endregion
     }
 }
