45dc509c1bc645fd10df979e3dbf26249c71e54d
Jonas Van der Aa <ketsukonn@gmail.com>
2020-03-21 19:02:03 +0100

    Web: Basic appgrid in reactstrap, pt. 1

---
+++ Landr.Web/wwwroot/react/AppGrid.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+
+class AppGrid extends React.Component {
+    render() {
+        const rows = this.props.rows
+
+        const rowTemplate = this.props.rowTemplate ? this.props.rowTemplate : (
+            <div className="row">
+
+            </div>
+            )
+             
+        if (rows && rows.length > 0) {
+
+        }
+        return (output)
+    }
+
+    
+}
---
+++ Landr.Web/wwwroot/react/BasicApp.jsx
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Button, CardImgOverlay
+} from 'reactstrap';
+
+class BasicApp extends Component {
+    render() {
+        const url = this.props.url;
+        const icon = this.props.icon;
+        const name = this.props.name;
+
+        const defaultContent = (
+            <div className="col-sm-4">
+                <a href={url}>
+                    <Card inverse>
+                        <CardImg width="100%" src={icon} alt={name} />
+                        <CardImgOverlay>
+                            <CardTitle>{name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </a>
+            </div>
+        );
+
+        return this.props.children ? this.props.children : defaultContent;
+    }
+}
