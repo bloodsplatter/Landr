cce534cf6a3c4ef676c51e3142330825788cc0da
Jonas Van der Aa <ketsukonn@gmail.com>
2020-10-06 18:05:46 +0200

    Add UserSettings

--- Landr.Data/LandrContext.cs
+++ Landr.Data/LandrContext.cs
@@ -13,19 +13,27 @@ namespace Landr.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             AppSetup(modelBuilder);
+            UserSettingsSetup(modelBuilder);
         }
 
-        private EntityTypeBuilder<BaseApp> AppSetup(ModelBuilder modelBuilder)
+        private void AppSetup(ModelBuilder modelBuilder)
         {
             var basicApp = modelBuilder.Entity<BaseApp>();
             basicApp.HasKey(ba => ba.Id);
 
             basicApp.Property(ba => ba.Name).IsRequired();
             basicApp.Property(ba => ba.Url).IsRequired();
+        }
 
-            return basicApp;
+        private void UserSettingsSetup(ModelBuilder modelBuilder)
+        {
+            var userSettings = modelBuilder.Entity<UserSettings>();
+            userSettings.HasKey(us => us.UserId);
+
+            userSettings.ToTable("UserSettings");
         }
 
         public DbSet<BaseApp> Apps { get; set; }
+        public DbSet<UserSettings> UserSettings { get; set; }
     }
 }
---
+++ Landr.Data/UserSettings.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Landr.Data
+{
+    public sealed class UserSettings
+    {
+        public string UserId { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
